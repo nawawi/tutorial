a99ac8362c71faf345fd423cf4e7ced88477f396
Nawawi Jamili <nawawi@rutweb.com>
2014-12-03 11:07:25 +0800

    updated

--- checklogin.php
+++ checklogin.php
@@ -77,14 +77,48 @@ $dblink->query($sql);
 //print_r($result);
 //echo "</pre>";
 
+$session_timeout = 0;
+$session_name = "TEST123";
+//$session_path = "C:/xampp/htdocs/test/sessions";
+$session_path = "/tmp/sessions";
+$session_cookie_secure = false;
 
+if ( $session_timeout == 0 ) {
+    ini_set("session.cache_expire", 600);
+    ini_set("session.gc_maxlifetime", 36000);
+    ini_set("session.cookie_lifetime", 36000);
+}
 
+if ( isset($session_name) && $session_name != '' ) {
+    ini_set('session.name', $session_name);
+}
 
+if ( isset($session_path) && is_dir($session_path) ) {
+    session_save_path($session_path);
+    ini_set('session.gc_probability', 1);
+}
 
+if ( $session_cookie_secure ) {
+    ini_set('session.cookie_secure','1');
+}
 
+// perlu letak setiap page
+session_start();
 
+// save data dari database ke session
+foreach( $data_last as $param => $value ) {
+    $_SESSION[$session_name][$param] = $value;
+}
 
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
 
+// clean session
+/*@unlink(session_save_path()."/sess_".session_id());
+@session_unset();
+@session_destroy();
+@session_write_close();*/
 
 
 
