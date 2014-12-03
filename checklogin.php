fdf7d83b71cb645fcdef4d9ce2840c1840afb4bd
Nawawi Jamili <nawawi@rutweb.com>
2014-12-03 10:33:50 +0800

    updated

--- checklogin.php
+++ checklogin.php
@@ -17,16 +17,46 @@ if ( !is_object($dblink) ) {
 }
 
 // check login dan password
-$sql = "select * from users where login='{$_POST['login']}' and password='".md5($_POST['password'])."' ";
+//$sql = "select * from users where login='{$_POST['login']}' and password='".md5($_POST['password'])."' ";
+
+// kalau combine table. masalah, data empty jika salah satu table empty.
+$sql = "select users.*,users_data.* from users,users_data where users.login='{$_POST['login']}' ";
+$sql .= "and users.password='".md5($_POST['password'])."' ";
+
 $result = $dblink->query($sql);
 if ( is_object($result) && $result->num_rows <= 0 ) {
-    echo "Login Id atau password tidak sah!";
+    echo "Login Id atau password tidak sah <a href='login.php'>Sila login semula</a>!";
     exit;
 }
 
+// get data
+$data = array();
+if ( is_object($result) ) {
+    while( $row = $result->fetch_object() ) {
+        //echo "<pre>";
+        //print_r($row);
+        //echo "</pre>";
+
+        $data['id'] = $row->id;
+        $data['login'] = $row->login;
+        $data['fullname'] = $row->fullname;
+        $data['lastlogin'] = $row->lastlogin;
+    }
+}
+
+// test data
 echo "<pre>";
-print_r($result);
+print_r($data);
 echo "</pre>";
+
+// update last login
+$lastlogin = date('Y-m-d H:i:s');
+$sql = "UPDATE users SET lastlogin='{$lastlogin}' WHERE id='{$data['id']}'";
+$dblink->query($sql);
+
+//echo "<pre>";
+//print_r($result);
+//echo "</pre>";
 
 
 
