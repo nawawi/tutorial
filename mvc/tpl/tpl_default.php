ab3414e0d7e046f734e15b655e1894289dad2ac7
Nawawi Jamili <nawawi@rutweb.com>
2014-12-04 12:40:42 +0800

    update MVC sample code/structure

--- mvc/tpl/tpl_default.php
+++ mvc/tpl/tpl_default.php
@@ -1,10 +1,25 @@
 <?php
 echo "FILE: ".__FILE__."<br>";
+echo "<br>";
+echo "Try browse mvc/users<br>";
+echo "Try browse mvc/users/details<br>";
+echo "Try browse mvc/users/save<br>";
+echo "Try browse mvc/users/update<br>";
+echo "Try browse mvc/users/delete<br>";
+
+echo "<br>";
+echo "<br>";
+echo "<b>TEST object \$this</b><br>";
 echo "<pre>";
 print_r($this);
 echo "</pre>";
 
 echo "<b>TEST DB</b><br>";
-
+echo "<pre>";
 $result = $this->db->query("select * from users");
 print_r($result);
+echo "</pre>";
+
+while( $row = $result->fetch_object() ) {
+    echo "<pre>".print_r($row,1)."</pre>";
+}
